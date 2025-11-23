11757a8e691a421697cc5c7157ab626dc2f0d1ee
Oscar <oscarcaicedo1085@gmail.com>
2025-11-23 18:43:28 -0500

    ola xd

---
+++ VideooJuegos/JuegoTienda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideooJuegos
+{
+    internal class JuegoTienda
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Plataforma { get; set; }
+        public string Genero { get; set; }
+        public double Precio { get; set; }
+        public string Imagen { get; set; }
+    }
+}
